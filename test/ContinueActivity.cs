146f9bf33c49252485de6e674e7075f444f685cf
unknown-user <unknown-user@fe80::d4f6:f53b:4450:cb2a%17>
2014-08-12 15:59:06 +0530

    Initial check-in of module test

---
+++ test/ContinueActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace test
+{
+	[Activity (Label = "@string/continueButton")]			
+	public class ContinueActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.continueLayout);
+
+			// Create your application here
+		}
+	}
+}
+
---
+++ test/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace test
+{
+	[Activity (Label = "test", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+		//	button.Click += delegate {
+				//button.Text = string.Format ("{0} clicks!", count++);
+		//	};
+
+			Button continueButton = FindViewById<Button> (Resource.Id.continueButton);
+
+			continueButton.Click += (object sender, EventArgs e) => {
+
+				var intent = new Intent(this, typeof(ContinueActivity));
+			};
+
+		
+		}
+		public void navigateNext(){
+			Intent intent = new Intent (this, typeof(ContinueActivity));
+		}
+		public void toContinue(View view){
+			//Intent intent = new Intent(getApplicationContext(), ContinueActivity);
+
+				StartActivity(typeof(ContinueActivity));
+
+		}
+	}
+}
+
+
+
